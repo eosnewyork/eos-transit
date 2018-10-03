3e71e9b18675e47fafddbff0abe45b780d6c6d1c
Vasilio Ruzanni <vasilioruzanni@gmail.com>
2018-10-03 08:22:17 +0300

    feat(core): Create a separate LogoutButton

---
+++ examples/ual-react-basic/src/core/UserMenuLogoutButton.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import styled from 'react-emotion';
+import { IoIosLogOut } from 'react-icons/io';
+
+export interface UserMenuLogoutButtonProps {
+  onClick?: (event: any) => void;
+}
+
+export const UserMenuLogoutButtonRoot = styled('div')({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  width: '100%',
+  border: 'none',
+  borderRadius: 1,
+  padding: '10px 20px',
+  fontSize: 14,
+  backgroundColor: '#20252f',
+  color: 'white',
+  textAlign: 'center',
+  outline: 'none',
+  transition: 'all 0.2s',
+
+  '&:hover': {
+    // backgroundColor: '#2e3542',
+    backgroundColor: '#98243f',
+    color: 'white',
+    cursor: 'pointer'
+  },
+
+  '&:active': {
+    // backgroundColor: '#2e3542',
+    backgroundColor: '#c3173f'
+  }
+});
+
+export const UserMenuLogoutButtonIcon = styled('div')({
+  paddingLeft: 10,
+  display: 'flex',
+  alignItems: 'center'
+});
+
+export const UserMenuLogoutButtonText = styled('div')({});
+
+export function UserMenuLogoutButton({ onClick }: UserMenuLogoutButtonProps) {
+  return (
+    <UserMenuLogoutButtonRoot onClick={onClick}>
+      <UserMenuLogoutButtonText>Logout</UserMenuLogoutButtonText>
+      <UserMenuLogoutButtonIcon>
+        <IoIosLogOut />
+      </UserMenuLogoutButtonIcon>
+    </UserMenuLogoutButtonRoot>
+  );
+}
+
+export default UserMenuLogoutButton;
