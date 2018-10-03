ca30af35b5c1ff336f44ef295698095e3aac6967
Vasilio Ruzanni <vasilioruzanni@gmail.com>
2018-10-03 08:23:17 +0300

    feat(shared): Provide accurate type definitions for wallet-related data

---
+++ examples/ual-react-basic/src/shared/wallets/types.ts
@@ -0,0 +1,28 @@
+export interface WalletConnectionStatus {
+  connecting?: boolean;
+  connected: boolean;
+  error?: boolean;
+  errorMessage?: string;
+}
+
+export interface WalletProviderInfo {
+  id: string;
+  name?: string;
+  description?: string;
+}
+
+export interface WalletInfo {
+  accountName: string;
+  accountAuthority?: string;
+  publicKey?: string;
+  eosBalance?: number;
+  ram?: number;
+  cpu?: number;
+  net?: number;
+}
+
+export interface WalletModel {
+  providerInfo: WalletProviderInfo;
+  connectionStatus: WalletConnectionStatus;
+  walletInfo?: WalletInfo;
+}
