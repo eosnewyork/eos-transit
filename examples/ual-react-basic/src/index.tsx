39b79b65da6f69133973442a91b92f400050f13f
Vasilio Ruzanni <vasilioruzanni@gmail.com>
2018-10-04 17:27:58 +0300

    feat(all): Use the Unstated provider to inject the shared SessionStateContainer instance

--- examples/ual-react-basic/src/index.tsx
+++ examples/ual-react-basic/src/index.tsx
@@ -1,20 +1,22 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
-// import { Provider as UnstatedProvider, Subscribe } from 'unstated';
+import { Provider as UnstatedProvider, Subscribe } from 'unstated';
 import 'minireset.css';
 import AppRoutes from './AppRoutes';
 import { applyGlobalStyles } from './globalStyles';
+import { SessionStateContainer } from './core/SessionStateContainer';
 
 applyGlobalStyles();
 
-// const sessionStateContainer = new SessionStateContainer();
-// const signUpStateContainer = new SignUpStateContainer();
+const sessionStateContainer = new SessionStateContainer();
 
 const Root = () => (
-  <Router>
-    <AppRoutes />
-  </Router>
+  <UnstatedProvider inject={[sessionStateContainer]}>
+    <Router>
+      <AppRoutes />
+    </Router>
+  </UnstatedProvider>
 );
 
 render(<Root />, document.getElementById('root'));
