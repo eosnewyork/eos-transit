9149aaec0d4d0ccefe4c5e1a2ba2b6d3cc71b0cb
Vasilio Ruzanni <vasilioruzanni@gmail.com>
2018-10-08 13:22:12 +0300

    refactor(login): Use the new provider list component instead of the old WalletSelectList

--- examples/ual-react-basic/src/login/LoginScreen.tsx
+++ examples/ual-react-basic/src/login/LoginScreen.tsx
@@ -2,9 +2,11 @@ import React, { Component } from 'react';
 import styled from 'react-emotion';
 import { Redirect } from 'react-router';
 // import { Subscribe } from 'unstated';
+import { CloseButton } from '../shared/buttons/CloseButton';
+import WalletListItem from '../shared/wallets/WalletListItem';
 import { WalletSelect } from './WalletSelect';
 import { LoginButton } from './LoginButton';
-import { CloseButton } from 'shared/buttons/CloseButton';
+import { LoginScreenWalletProviders } from './LoginScreenWalletProviders';
 
 // Visual components
 
@@ -55,6 +57,28 @@ const ContentPanelHeading = styled('span')({
   fontWeight: 300
 });
 
+// TEMP
+const walletProviders = [
+  {
+    id: 'scatter-desktop',
+    name: 'Scatter Desktop',
+    description:
+      'Scatter Desktop application that keeps your private keys secure'
+  },
+  {
+    id: 'eos-metro',
+    name: 'METRO™ Hardware Wallet',
+    description:
+      'Use secure hardware private key vault to sign your transactions'
+  },
+  {
+    id: 'paste-the-private-key',
+    name: 'Paste-The-Private-Key™',
+    description:
+      'Forget about security and just paste your private key directly to sign your transactions'
+  }
+];
+
 // tslint:disable-next-line:no-empty-interface
 export interface LoginScreenProps {
   // sessionStateContainer: SessionStateContainer;
@@ -78,8 +102,17 @@ export class LoginScreen extends Component<LoginScreenProps, LoginScreenState> {
     // return this.props.sessionStateContainer.login(loginFormModel);
   };
 
+  getAvailableWalletProviders = () => {
+    // TODO: Will be taken from configured UAL instance directly
+    return walletProviders;
+  };
+
   render() {
-    const { switchScreen, handleWalletSelect } = this;
+    const {
+      switchScreen,
+      getAvailableWalletProviders,
+      handleWalletSelect
+    } = this;
     const { showLoginOptions } = this.state;
     // const { state, isAuthenticated } = props.sessionStateContainer;
     // const { isAuthenticating } = state;
@@ -92,15 +125,15 @@ export class LoginScreen extends Component<LoginScreenProps, LoginScreenState> {
           <>
             <ContentPanelHeader>
               <ContentPanelHeaderItem main={true}>
-                <ContentPanelHeading>
-                  Login Options
-                </ContentPanelHeading>
+                <ContentPanelHeading>Login Options</ContentPanelHeading>
               </ContentPanelHeaderItem>
               <ContentPanelHeaderItem alignEnd={true}>
                 <CloseButton onClick={switchScreen} size={40} />
               </ContentPanelHeaderItem>
             </ContentPanelHeader>
-            <WalletSelect onWalletSelect={handleWalletSelect} />
+            <LoginScreenWalletProviders
+              walletProviders={getAvailableWalletProviders()}
+            />
           </>
         ) : (
           <LoginButton onClick={switchScreen} />
