96c31741ca1825f84e82dfbacb79adb7267014d7
Vasilio Ruzanni <vasilioruzanni@gmail.com>
2018-10-08 14:11:27 +0300

    refactor(login): Refactor WalletProviders to a more generic WalletList

---
+++ examples/ual-react-basic/src/login/LoginScreenWalletList.tsx
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import styled from 'react-emotion';
+import WalletListItem from '../shared/wallets/WalletListItem';
+import { WalletProviderInfo, WalletModel } from 'shared/wallets/types';
+
+// Visual components
+
+const LoginScreenWalletListRoot = styled('div')({
+  display: 'flex',
+  flexDirection: 'column',
+  width: '100%',
+  marginBottom: 4
+});
+
+// Exported component
+
+interface LoginScreenWalletListProps {
+  wallets: WalletModel[];
+  onWalletSelect?: (wallet: WalletModel) => void;
+}
+
+interface LoginScreenWalletListState {
+  wallets: WalletModel[];
+}
+
+export class LoginScreenWalletList extends Component<
+  LoginScreenWalletListProps,
+  LoginScreenWalletListState
+> {
+  constructor(props: LoginScreenWalletListProps) {
+    super(props);
+    this.state = { wallets: props.wallets };
+  }
+
+  handleWalletSelect = (wallet: WalletModel) => {
+    const { onWalletSelect } = this.props;
+    if (typeof onWalletSelect === 'function') {
+      onWalletSelect(wallet);
+    }
+  }
+
+  render() {
+    const { handleWalletSelect } = this;
+    const { wallets } = this.state;
+
+    return (
+      <LoginScreenWalletListRoot>
+        {wallets.map(wallet => (
+          <WalletListItem
+            key={wallet.providerInfo.id}
+            onSelect={handleWalletSelect}
+            data={wallet}
+            large={true}
+          />
+        ))}
+      </LoginScreenWalletListRoot>
+    );
+  }
+}
+
+export default LoginScreenWalletList;
