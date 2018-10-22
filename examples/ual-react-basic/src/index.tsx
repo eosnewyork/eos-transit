faf1b6a16aeab2086847ce8154a7fea9383e8626
Vasilio Ruzanni <vasilioruzanni@gmail.com>
2018-10-22 14:46:13 +0300

    fix(ual-react-basic): Add missing example files

--- examples/ual-react-basic/src/index.tsx
+++ examples/ual-react-basic/src/index.tsx
@@ -3,7 +3,7 @@ import { render } from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider as UnstatedProvider } from 'unstated';
 import { makeUAL } from 'eos-ual';
-import { makeScatterDesktopIntegration } from 'eos-ual/lib/integrations/scatter-desktop';
+import { makeScatterDesktopWalletProvider } from 'eos-ual/lib/walletProviders/scatter-desktop';
 import 'minireset.css';
 import AppRoutes from './AppRoutes';
 import { applyGlobalStyles } from './globalStyles';
@@ -22,17 +22,16 @@ const scatterNetworkConfig = {
   chainId: 'cf057bbfb72640471fd910bcb67639c22df9f92470936cddc1ade0e2f2e7dc4f'
 };
 
-const scatterDesktopIntegration = makeScatterDesktopIntegration(
+const scatterDesktopWalletProvider = makeScatterDesktopWalletProvider(
   appName,
   scatterNetworkConfig
 );
 
 const opts = {
   appName,
+  eosRpcUrl: 'http://api.pennstation.eosnewyork.io:7001',
   chainId: 'cf057bbfb72640471fd910bcb67639c22df9f92470936cddc1ade0e2f2e7dc4f',
-  httpEndpoint: 'http://api.pennstation.eosnewyork.io:7001',
-  expireInSeconds: 60,
-  integrations: [scatterDesktopIntegration]
+  walletProviders: [scatterDesktopWalletProvider]
 };
 
 const ualInstance = makeUAL(opts);
