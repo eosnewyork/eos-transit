3db3e3c64a54b3bc12e9807eff3f9eaaab8851a2
Vasilio Ruzanni <vasilioruzanni@gmail.com>
2018-10-22 14:37:40 +0300

    chore(core): Fix the defaultOptions param name

--- src/defaultOptions.ts
+++ src/defaultOptions.ts
@@ -1,7 +1,7 @@
 import { UALOptions } from './types';
 
 const defaultOptions: Partial<UALOptions> = {
-  httpEndpoint: 'http://127.0.0.1:8888',
+  eosRpcUrl: 'http://127.0.0.1:8888',
   expireInSeconds: 60
 };
 
