42da9322958c5202688553dee57a560b7ea40970
Vasilio Ruzanni <vasilioruzanni@gmail.com>
2018-10-29 16:19:47 +0300

    chore(core): Cleanup the old defaultSettings module, redundant now

--- src/defaultOptions.ts
+++
@@ -1,8 +0,0 @@
-import { UALOptions } from './types';
-
-const defaultOptions: Partial<UALOptions> = {
-  eosRpcUrl: 'http://127.0.0.1:8888',
-  expireInSeconds: 60
-};
-
-export default defaultOptions;
