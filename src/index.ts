86b0d2b120faefa076be6c89016b1d40ec1e3a7e
Vasilio Ruzanni <vasilioruzanni@gmail.com>
2018-10-29 15:40:59 +0300

    feat(core): Export necessary stuff and support implicit default context

--- src/index.ts
+++ src/index.ts
@@ -1,78 +1,17 @@
-import { Api, JsonRpc } from 'eosjs';
-import { TextEncoder, TextDecoder } from 'text-encoding';
-import { UALOptions, UALInstance } from './types';
+import { WalletAccessContext, WalletAccessContextOptions } from './types';
+import { initAccessContext } from './walletAccessContext';
+
 export * from './types';
+export { initAccessSession } from './walletAccessSession';
+export { initAccessContext };
 
-function transfer(
-  eosApi: Api,
-  senderName: string,
-  receiverName: string,
-  amount: number,
-  memo: string = ''
-) {
-  return new Promise((resolve, reject) => {
-    if (!senderName) {
-      return reject(new Error('Sender account name is not provided!'));
-    }
-    if (!receiverName) {
-      return reject(new Error('Receiver account name is not provided!'));
-    }
+export let defaultAccessContext: WalletAccessContext | undefined = void 0;
 
-    if (!amount) return reject(new Error('Amount not specified'));
-
-    return eosApi
-      .transact({
-        actions: [
-          {
-            account: senderName,
-            name: 'transfer',
-            authorization: [
-              {
-                actor: senderName,
-                permission: 'active'
-              }
-            ],
-            data: {
-              from: senderName,
-              to: receiverName,
-              quantity: `${Number(amount).toFixed(4)} EOS`,
-              memo
-            }
-          }
-        ]
-      })
-      .then(resolve)
-      .catch(reject);
-  });
+export function initDefaultAccessContext(
+  options: WalletAccessContextOptions
+): WalletAccessContext {
+  defaultAccessContext = initAccessContext(options);
+  return defaultAccessContext;
 }
 
-export function makeUAL(options: UALOptions): UALInstance {
-  const { walletProviders } = options;
-  const selectedProvider = walletProviders[0];
-
-  const eosJsonRpc = new JsonRpc(options.eosRpcUrl, { fetch });
-
-  // Note: The EOS instance is per-wallet-provider
-  // (would be created upon user selecting one)
-  const eosApi = new Api({
-    rpc: eosJsonRpc,
-    chainId: options.chainId,
-    signatureProvider: selectedProvider.signatureProvider,
-    textEncoder: new TextEncoder(),
-    textDecoder: new TextDecoder()
-  });
-
-  return {
-    connect(accountName?: string) {
-      return selectedProvider.connect(accountName);
-    },
-
-    getAccount(accountName: string) {
-      return eosApi.rpc.get_account(accountName);
-    },
-
-    transfer(from: string, to: string, amount: number, memo?: string) {
-      return transfer(eosApi, from, to, amount, memo);
-    }
-  };
-}
+export default defaultAccessContext;
