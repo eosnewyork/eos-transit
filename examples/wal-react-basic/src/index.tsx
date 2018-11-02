975a55eef5798ee92818ca6c283b3d39a1ccb18f
Vasilio Ruzanni <vasilioruzanni@gmail.com>
2018-11-02 19:04:36 +0300

    feat(examples): Make entire wal-react-basic app subscribed to a default access context updates

--- examples/wal-react-basic/src/index.tsx
+++ examples/wal-react-basic/src/index.tsx
@@ -5,13 +5,18 @@ import 'minireset.css';
 import './initDefaultAccessContext';
 import AppRoutes from './AppRoutes';
 import { applyGlobalStyles } from './globalStyles';
+import AccessContextSubscribe from 'AccessContextSubscribe';
 
 applyGlobalStyles();
 
 const Root = () => (
-  <Router>
-    <AppRoutes />
-  </Router>
+  <AccessContextSubscribe>
+    {() => (
+      <Router>
+        <AppRoutes />
+      </Router>
+    )}
+  </AccessContextSubscribe>
 );
 
 render(<Root />, document.getElementById('root'));
