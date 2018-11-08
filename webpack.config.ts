c593629e27a0b1e61760aedc488055c2fee0dd95
Vasilio Ruzanni <vasilioruzanni@gmail.com>
2018-11-08 15:22:21 +0300

    feat(config): Provide the webpack configuration to bundle the stuff nicely

---
+++ webpack.config.ts
@@ -0,0 +1,73 @@
+import path from 'path';
+import { Configuration, ProvidePlugin } from 'webpack';
+
+const commonConfig: Configuration = {
+  mode: 'production',
+  // entry: {
+  //   'wal-eos': './src/index.ts',
+  //   'wal-eos-scatter': './src/walletProviders/scatter/index.ts',
+  //   'wal-eos-stub': './src/walletProviders/stub.ts'.
+  // },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: {
+          loader: 'ts-loader',
+          options: {
+            configFile: 'tsconfig.webpack.json'
+          }
+        },
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  plugins: [
+    new ProvidePlugin({
+      'window.ScatterJS': ['scatterjs-core', 'default'],
+      'window.ScatterEOS': ['scatterjs-plugin-eosjs2', 'default']
+    })
+  ],
+  externals: {
+    'scatterjs-core': 'ScatterJS',
+    'scatterjs-plugin-eosjs2': 'ScatterEOS'
+  },
+  stats: {
+    colors: true
+  }
+};
+
+const libConfig: Configuration = {
+  ...commonConfig,
+  entry: {
+    'wal-eos': './src/index.ts'
+  },
+  output: {
+    filename: '[name].min.js',
+    path: path.resolve(__dirname, 'dist'),
+    libraryTarget: 'umd',
+    library: 'WAL',
+    libraryExport: 'default'
+  }
+};
+
+const providersConfig: Configuration = {
+  ...commonConfig,
+  entry: {
+    scatter: './src/walletProviders/scatter/index.ts',
+    stub: './src/walletProviders/stub.ts'
+  },
+  output: {
+    filename: 'wal-eos-[name].min.js',
+    path: path.resolve(__dirname, 'dist'),
+    libraryTarget: 'umd',
+    library: '[name]',
+    libraryExport: 'default',
+    globalObject: 'window.WAL'
+  }
+};
+
+export default [libConfig, providersConfig];
