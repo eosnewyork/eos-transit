6097dbf992da7f4bd9005db2dcf2cb30776a2b86
Warrick FitzGerald <warrick@eosnewyork.io>
2019-03-19 14:15:50 -0400

    Fixed the stub provider and adjusted the ledger umd to ref eosjs correctly

--- packages/eos-transit-ledger-provider/webpack.config.ts
+++ packages/eos-transit-ledger-provider/webpack.config.ts
@@ -2,47 +2,42 @@ import path from 'path';
 import { Configuration, ProvidePlugin } from 'webpack';
 
 const config: Configuration = {
-  mode: 'production',
-  entry: './src/index.ts',
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: {
-          loader: 'ts-loader',
-          options: {
-            configFile: 'tsconfig.webpack.json'
-          }
-        },
-        exclude: /node_modules/
-      }
-    ]
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js']
-  },
-  output: {
-    filename: 'eos-transit-ledger-provider.min.js',
-    path: path.resolve(__dirname, 'umd'),
-    libraryTarget: 'umd',
-    library: ['WAL', 'providers', 'ledger'],
-    libraryExport: 'default'
-  },
-  plugins: [
-    new ProvidePlugin(
-    	{
-          'window.WAL': ['eos-transit', 'default'],
-          '_hwTransport2': ['@babel/runtime/regenerator']
-	    }
-    )
-  ],
-  externals: {
-    'eos-transit':'WAL',
-    'eosjs' : 'rpc'
-  },
-  stats: {
-    colors: true
-  }
+	mode: 'production',
+	entry: './src/index.ts',
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				use: {
+					loader: 'ts-loader',
+					options: {
+						configFile: 'tsconfig.webpack.json'
+					}
+				},
+				exclude: /node_modules/
+			}
+		]
+	},
+	resolve: {
+		extensions: [ '.tsx', '.ts', '.js' ]
+	},
+	output: {
+		filename: 'eos-transit-ledger-provider.min.js',
+		path: path.resolve(__dirname, 'umd'),
+		libraryTarget: 'umd',
+		library: [ 'WAL', 'providers', 'ledger' ],
+		libraryExport: 'default'
+	},
+	plugins: [
+		new ProvidePlugin({
+			'window.WAL': [ 'eos-transit', 'default' ],
+			_hwTransport2: [ '@babel/runtime/regenerator' ]
+		})
+	],
+	externals: 'eosjs',
+	stats: {
+		colors: true
+	}
 };
 
 export default config;
--- packages/eos-transit-stub-provider/src/index.ts
+++ packages/eos-transit-stub-provider/src/index.ts
@@ -1,89 +1,112 @@
 import { ApiInterfaces, RpcInterfaces } from 'eosjs';
-import { WalletProvider, NetworkConfig, WalletAuth } from 'eos-transit';
+import { WalletProvider, NetworkConfig, WalletAuth, DiscoveryOptions } from 'eos-transit';
 
 // A fake wallet provider that does nothing and always
 // errors on connection attempts. Useful for demoing.
 
-export function makeSignatureProvider() {
-  return {
-    async getAvailableKeys() {
-      return [];
-    },
+let accountPublickey: string;
 
-    async sign(signatureProviderArgs: ApiInterfaces.SignatureProviderArgs): Promise<RpcInterfaces.PushTransactionArgs> {
-      // This is where you'd implement your custom signature provider
-      var signatureArray = [""];
-      var txn = new Uint8Array(10); 
-      
-      var respone : RpcInterfaces.PushTransactionArgs =  {
-        signatures : signatureArray,
-        serializedTransaction : txn
-      };
+function logIfDebugEnabled(msg: string) {
+	const debug = localStorage.getItem('DEBUG');
+	if (debug === 'true') {
+		console.log('IN WALLET: ' + msg);
+	}
+}
 
-      return respone;
-      
-    }
-  };
+function makeSignatureProvider(network: NetworkConfig) {
+	return {
+		async getAvailableKeys() {
+			logIfDebugEnabled('In getAvailableKeys');
+
+			logIfDebugEnabled('Return Key: ' + accountPublickey);
+			const arr: string[] = [ accountPublickey ];
+			return arr;
+		},
+
+		async sign(
+			signatureProviderArgs: ApiInterfaces.SignatureProviderArgs
+		): Promise<RpcInterfaces.PushTransactionArgs> {
+			logIfDebugEnabled('In Sign');
+
+			const signatureArray = [ '' ];
+			const respone: RpcInterfaces.PushTransactionArgs = {
+				signatures: signatureArray,
+				serializedTransaction: signatureProviderArgs.serializedTransaction
+			};
+
+			return respone;
+		}
+	};
+}
+
+function discover(discoveryOptions: DiscoveryOptions) {
+	logIfDebugEnabled('The discover() method of myWallet was called');
+
+	// You probably do not need to implement this method.
+
+	return new Promise((resolve, reject) => {
+		const discoveryInfo = {
+			keys: [],
+			note: 'Wallet does not support discovery'
+		};
+
+		resolve(discoveryInfo);
+	});
 }
 
 export interface StubWalletProviderOptions {
-  id: string;
-  name: string;
-  shortName: string;
-  description?: string;
-  errorTimeout?: number;
+	id: string;
+	name: string;
+	shortName: string;
+	description?: string;
+	errorTimeout?: number;
 }
 
-export function stubWalletProvider({
-  id,
-  name,
-  shortName,
-  description,
-  errorTimeout
-}: StubWalletProviderOptions) {
-  return function makeWalletProvider(network: NetworkConfig): WalletProvider {
-    function connect(appName: string) {
-      return new Promise((resolve, reject) => {
-        setTimeout(() => {
-          reject(`Cannot connect to "${shortName}" wallet provider`);
-        }, errorTimeout || 2500);
-      });
-    }
+export function stubWalletProvider({ id, name, shortName, description, errorTimeout }: StubWalletProviderOptions) {
+	return function makeWalletProvider(network: NetworkConfig): WalletProvider {
+		function connect(appName: string) {
+			return new Promise((resolve, reject) => {
+				setTimeout(() => {
+					reject(`Cannot connect to "${shortName}" wallet provider`);
+				}, errorTimeout || 2500);
+			});
+		}
 
-    function disconnect(): Promise<any> {
-      return Promise.resolve();
-    }
+		function disconnect(): Promise<any> {
+			return Promise.resolve();
+		}
 
-    // Authentication
+		// Authentication
 
-    async function login(accountName?: string): Promise<WalletAuth> {
-      return new Promise<WalletAuth>((resolve, reject) => {
-        setTimeout(() => {
-          reject(`Cannot login to "${shortName}" wallet provider`);
-        }, errorTimeout || 2500);
-      });
-    }
+		async function login(accountName?: string): Promise<WalletAuth> {
+			return new Promise<WalletAuth>((resolve, reject) => {
+				setTimeout(() => {
+					reject(`Cannot login to "${shortName}" wallet provider`);
+				}, errorTimeout || 2500);
+			});
+		}
 
-    function logout(accountName?: string): Promise<boolean> {
-      return Promise.resolve(true);
-    }
+		function logout(accountName?: string): Promise<boolean> {
+			return Promise.resolve(true);
+		}
 
-    const walletProvider: WalletProvider = {
-      id,
-      meta: {
-        name,
-        shortName,
-        description
-      },
-      signatureProvider: makeSignatureProvider(),
-      connect,
-      disconnect,
-      login,
-      logout
-    };
+		const walletProvider: WalletProvider = {
+			id,
+			meta: {
+				name,
+				shortName,
+				description
+			},
+			signatureProvider: makeSignatureProvider(network),
+			connect,
+			discover,
+			disconnect,
+			login,
+			logout
+		};
 
-    return walletProvider;
-  };
+		return walletProvider;
+	};
 }
 
 export default stubWalletProvider;
