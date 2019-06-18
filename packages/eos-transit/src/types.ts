d25900771e4e8c894e9eb0f21739b43da402a15b
Tom Keidar <tom.keidar@portis.io>
2019-06-18 18:10:16 +0300

    implement optional show provider method

--- packages/eos-transit/src/types.ts
+++ packages/eos-transit/src/types.ts
@@ -3,38 +3,38 @@ import { ApiInterfaces, JsonRpc, Api } from 'eosjs';
 // Core helper types
 
 export interface AccountInfo {
-	name: string;
-	publicKey: string;
-	core_liquid_balance?: string;
-	ram_quota?: number;
-	cpu_limit: {
-		available: number;
-	};
-	net_limit: {
-		available: number;
-	};
+  name: string;
+  publicKey: string;
+  core_liquid_balance?: string;
+  ram_quota?: number;
+  cpu_limit: {
+    available: number;
+  };
+  net_limit: {
+    available: number;
+  };
 
-	// TODO: permissions and more
+  // TODO: permissions and more
 }
 
 export interface DiscoveryData {
-	keyToAccountMap: DiscoveryAccount[];
-	keys?: { index: number; key: string }[];
+  keyToAccountMap: DiscoveryAccount[];
+  keys?: { index: number; key: string }[];
 }
 
 export interface DiscoveryAccount {
-	index: number;
-	key: string;
-	accounts: {
-		account: string;
-		authorization: string;
-	}[];
+  index: number;
+  key: string;
+  accounts: {
+    account: string;
+    authorization: string;
+  }[];
 }
 
 export interface WalletAuth {
-	accountName: string;
-	permission: string;
-	publicKey: string;
+  accountName: string;
+  permission: string;
+  publicKey: string;
 }
 
 // State management
@@ -50,9 +50,9 @@ export type StateListener<TState> = (newState: TState | undefined) => void;
 export type StateUnsubscribeFn = () => void;
 
 export interface StateContainer<TState = any> {
-	getState: () => TState | undefined;
-	updateState: UpdateStateFn<TState>;
-	subscribe: (listener: StateListener<TState>) => StateUnsubscribeFn;
+  getState: () => TState | undefined;
+  updateState: UpdateStateFn<TState>;
+  subscribe: (listener: StateListener<TState>) => StateUnsubscribeFn;
 }
 
 // Util
@@ -64,31 +64,37 @@ export type UnsubscribeFn = () => void;
 // Network
 
 export interface NetworkConfig {
-	name?: string;
-	protocol?: string;
-	host: string;
-	port?: number;
-	chainId: string;
+  name?: string;
+  protocol?: string;
+  host: string;
+  port?: number;
+  chainId: string;
 }
 
 // Wallet Providers
 
 export interface WalletProviderMetadata {
-	name?: string;
-	shortName?: string;
-	description?: string;
+  name?: string;
+  shortName?: string;
+  description?: string;
 }
 
 export interface WalletProvider {
-	id: string;
-	meta?: WalletProviderMetadata;
-	signatureProvider: ApiInterfaces.SignatureProvider;
-	connect(appName: string): Promise<any>;
-	discover(discoveryOptions: DiscoveryOptions): Promise<any>;
-	disconnect(): Promise<any>;
-	login(accountName?: string, authorization?: string, index?: number, key?: string): Promise<WalletAuth>;
-	logout(accountName?: string): Promise<any>;
-	signArbitrary(data: string, userMessage: string): Promise<string>;
+  id: string;
+  meta?: WalletProviderMetadata;
+  signatureProvider: ApiInterfaces.SignatureProvider;
+  connect(appName: string): Promise<any>;
+  discover(discoveryOptions: DiscoveryOptions): Promise<any>;
+  disconnect(): Promise<any>;
+  login(
+    accountName?: string,
+    authorization?: string,
+    index?: number,
+    key?: string
+  ): Promise<WalletAuth>;
+  logout(accountName?: string): Promise<any>;
+  signArbitrary(data: string, userMessage: string): Promise<string>;
+  show(): Promise<void>;
 }
 
 export type MakeWalletProviderFn = (network: NetworkConfig) => WalletProvider;
@@ -96,80 +102,80 @@ export type MakeWalletProviderFn = (network: NetworkConfig) => WalletProvider;
 // State-tracket Wallet instance
 
 export interface WalletState {
-	connecting?: boolean;
-	connected?: boolean;
-	connectionError?: boolean;
-	connectionErrorMessage?: string;
-	auth?: WalletAuth;
-	authenticating?: boolean;
-	authenticated?: boolean;
-	authenticationConfirmed?: boolean;
-	authenticationError?: boolean;
-	authenticationErrorMessage?: string;
-	accountInfo?: AccountInfo;
-	accountFetching?: boolean;
-	accountFetchError?: boolean;
-	accountFetchErrorMessage?: string;
+  connecting?: boolean;
+  connected?: boolean;
+  connectionError?: boolean;
+  connectionErrorMessage?: string;
+  auth?: WalletAuth;
+  authenticating?: boolean;
+  authenticated?: boolean;
+  authenticationConfirmed?: boolean;
+  authenticationError?: boolean;
+  authenticationErrorMessage?: string;
+  accountInfo?: AccountInfo;
+  accountFetching?: boolean;
+  accountFetchError?: boolean;
+  accountFetchErrorMessage?: string;
 }
 
 //{ pathIndexList: [ 0, 1, 2, 35 ] }
 export interface DiscoveryOptions {
-	pathIndexList: number[];
+  pathIndexList: number[];
 }
 
 export interface Wallet {
-	_instanceId: string; // UUID, for internal purposes
-	ctx: WalletAccessContext;
-	state: WalletState;
-	provider: WalletProvider;
-	eosApi: Api;
-	auth?: WalletAuth;
-	accountInfo?: AccountInfo;
-	connected: boolean;
-	authenticated: boolean;
-	inProgress: boolean;
-	active: boolean;
-	hasError: boolean;
-	errorMessage?: string;
-	connect(): Promise<any>;
-	discover(discoveryOptions: DiscoveryOptions): Promise<any>;
-	disconnect(): Promise<any>;
-	login(accountName?: string, authorization?: string): Promise<AccountInfo>;
-	logout(accountName?: string): Promise<any>;
-	fetchAccountInfo(accountName?: string): Promise<AccountInfo>;
-	terminate(): Promise<boolean>;
-	subscribe(listener: StateListener<WalletState>): StateUnsubscribeFn;
-	signArbitrary(data: string, userMessage: string): Promise<string>;
+  _instanceId: string; // UUID, for internal purposes
+  ctx: WalletAccessContext;
+  state: WalletState;
+  provider: WalletProvider;
+  eosApi: Api;
+  auth?: WalletAuth;
+  accountInfo?: AccountInfo;
+  connected: boolean;
+  authenticated: boolean;
+  inProgress: boolean;
+  active: boolean;
+  hasError: boolean;
+  errorMessage?: string;
+  connect(): Promise<any>;
+  discover(discoveryOptions: DiscoveryOptions): Promise<any>;
+  disconnect(): Promise<any>;
+  login(accountName?: string, authorization?: string): Promise<AccountInfo>;
+  logout(accountName?: string): Promise<any>;
+  fetchAccountInfo(accountName?: string): Promise<AccountInfo>;
+  terminate(): Promise<boolean>;
+  subscribe(listener: StateListener<WalletState>): StateUnsubscribeFn;
+  signArbitrary(data: string, userMessage: string): Promise<string>;
 }
 
 // Wallet access context
 
 export interface WalletAccessContextOptions {
-	appName: string;
-	network: NetworkConfig;
-	walletProviders: MakeWalletProviderFn[];
+  appName: string;
+  network: NetworkConfig;
+  walletProviders: MakeWalletProviderFn[];
 }
 
 export interface WalletAccessContextState {
-	wallets: Wallet[];
+  wallets: Wallet[];
 }
 
 export interface WalletAccessContext {
-	appName: string;
-	eosRpc: JsonRpc;
-	network: NetworkConfig;
-	initWallet(walletProvider: WalletProvider | string): Wallet;
-	addWalletProvider(walletProvider: MakeWalletProviderFn): void;
-	getWalletProviders(): WalletProvider[];
-	getWallets(): Wallet[];
-	getActiveWallets(): Wallet[];
-	// updateWalletState(wallet: Wallet): void;
-	detachWallet(wallet: Wallet): void;
-	logoutAll(): Promise<boolean>;
-	disconnectAll(): Promise<boolean>;
-	terminateAll(): Promise<boolean>;
-	destroy(): Promise<any>;
-	subscribe(listener: Listener<WalletAccessContext>): UnsubscribeFn;
+  appName: string;
+  eosRpc: JsonRpc;
+  network: NetworkConfig;
+  initWallet(walletProvider: WalletProvider | string): Wallet;
+  addWalletProvider(walletProvider: MakeWalletProviderFn): void;
+  getWalletProviders(): WalletProvider[];
+  getWallets(): Wallet[];
+  getActiveWallets(): Wallet[];
+  // updateWalletState(wallet: Wallet): void;
+  detachWallet(wallet: Wallet): void;
+  logoutAll(): Promise<boolean>;
+  disconnectAll(): Promise<boolean>;
+  terminateAll(): Promise<boolean>;
+  destroy(): Promise<any>;
+  subscribe(listener: Listener<WalletAccessContext>): UnsubscribeFn;
 }
 
 // Transactions
