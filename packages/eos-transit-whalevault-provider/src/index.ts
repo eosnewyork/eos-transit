1484e7843374dd36dbad4c1eef35f1d6cb27a3f4
Warrick FitzGerald <warrick@eosnewyork.io>
2019-06-22 21:11:45 -0400

    Support for multiple accounts

--- packages/eos-transit-whalevault-provider/src/index.ts
+++ packages/eos-transit-whalevault-provider/src/index.ts
@@ -1,24 +1,6 @@
 import { Api, ApiInterfaces, RpcInterfaces, JsonRpc } from 'eosjs';
 import { WalletProvider, NetworkConfig, WalletAuth, DiscoveryOptions } from 'eos-transit';
 
-interface WhaleVaultState {
-	accountPublickey: string,  // active pubkey
-	accountPubKeys: {},
-	accountChain: string,
-	accountName: string,
-	network?: NetworkConfig,
-	appId: string,
-	whalevault: any
-}
-const wv: WhaleVaultState = {
-	accountPublickey: '',
-	accountPubKeys: {},
-	accountChain: '',
-	accountName: '',
-	appId: '',
-	whalevault: null
-}
-
 declare global {
     interface Window { whalevault: any; }
 }
@@ -32,86 +14,37 @@ function logIfDebugEnabled(msg: string) {
 	}
 }
 
-function makeSignatureProvider(network: NetworkConfig) {
-	wv.network = network;
-	return {
-		async getAvailableKeys() {
-			logIfDebugEnabled('In getAvailableKeys');
-
-			logIfDebugEnabled('Return Active Key: ' + wv.accountPublickey);
-			const arr: string[] = [ wv.accountPublickey ];
-			return arr;
-		},
-
-		async sign(
-			signatureProviderArgs: ApiInterfaces.SignatureProviderArgs,
-			reason?: string
-		): Promise<RpcInterfaces.PushTransactionArgs> {
-			logIfDebugEnabled('In Sign');
-
-			const rpc = new JsonRpc(network.protocol + '://' + network.host + ':' + network.port);
-			const args = { rpc, 
-                           authorityProvider: undefined, 
-                           abiProvider: undefined, 
-                           signatureProvider: this, 
-                           chainId: undefined, 
-                           textEncoder: undefined, 
-                           textDecoder: undefined
-                         };
-			const api = new Api(args);
-			const _txn = await api.deserializeTransactionWithActions(signatureProviderArgs.serializedTransaction);
-
-			if (reason == null || reason === '') reason = _txn.actions[0].name;
-
-			_txn.network = network;
-
-			const response = await wv.whalevault.promiseRequestSignBuffer(wv.appId, wv.accountChain+':'+wv.accountName, 
-                                                                          _txn, 'active', reason, 'eos');
-
-			const signatureArray = response.success ? [response.result] : [];
-			const sigResponse: RpcInterfaces.PushTransactionArgs = {
-				signatures: signatureArray,
-				serializedTransaction: signatureProviderArgs.serializedTransaction
-			};
-
-			return sigResponse;
-		}
-	};
+export interface matchedIndexItem {
+	accountName: string;
+	key: string;
 }
 
-function signArbitrary(data: string, userMessage: string): Promise<string> {
-	return new Promise((resolve, reject) => {
-		if (wv.appId === '') reject('uninitalized');
-		wv.whalevault.requestSignBuffer(wv.appId, wv.accountChain+':'+wv.accountName, data, 'active',
-                                        userMessage, 'eos', (response: any) => {
-			if (response.success) resolve(response.result); else reject(response.message);
-		});
-	});
-}
 
 export function whalevaultWalletProvider() {
+
+
 	return function makeWalletProvider(network: NetworkConfig): WalletProvider {
-		// Connection
+		let keyMap: matchedIndexItem[] = [];
+
+		let whalevault: any;
+		let appId: string;
+		let connected: boolean;
+		
 
 		function connect(appName: string): Promise<any> {
 			logIfDebugEnabled('The connect method of transit:WhaleVault was called');
-			console.log(window.whalevault);
-			console.log(wv);
-			logIfDebugEnabled('done');
-
+			
 			if (!appName || appName === '') throw new Error('appName required');
 
 			const res = async function m2(): Promise<any> {
-				console.log('her1');
-				if (wv.whalevault) return true;
-				console.log('her2');
-				// await new Promise((resolve, reject) => { window.onload = resolve; });
-				console.log('her3');
+				console.log(connected);
+				if (connected) return true;
 				if (typeof window.whalevault !== 'undefined') {
 					const response = await window.whalevault.promiseRequestHandshake(appName);
 					if (response) {
-						wv.appId = appName;
-						wv.whalevault = window.whalevault;
+						connected = true;
+						appId = appName;
+						whalevault = window.whalevault;
 						return true; 
 					} else return false;
 				}
@@ -134,11 +67,7 @@ export function whalevaultWalletProvider() {
 		}
 
 		function disconnect(): Promise<any> {
-			wv.accountPublickey = '';
-			wv.accountPubKeys = '';
-			wv.accountChain = '';
-			wv.accountName = '';
-			wv.appId = '';
+			keyMap = [];
 			return Promise.resolve(true);
 		}
 
@@ -146,47 +75,33 @@ export function whalevaultWalletProvider() {
 		async function login(accountName?: string): Promise<WalletAuth> {
 			logIfDebugEnabled('The login method of transit:WhaleVault was called');
 
-			if (wv.appId === '') throw new Error('uninitalized');
-			if (accountName == null) throw new Error('chainid:userid must be provided');
-			
-			const arrAccount = accountName.split(":");
+			let wvAccountName = '';
+			let wvAccountKey = '';
+
+			//@ts-ignore
+			let response = await whalevault.promiseRequestPubKeys(appId, "eos:");
+
+			const arrAccount = response.data.username.split(":");
 			if (arrAccount.length > 1) {
-				wv.accountChain = arrAccount[0];
-				wv.accountName = arrAccount[1];
+				wvAccountName = arrAccount[1];
 			} else {
-				wv.accountChain = 'eos'
-				wv.accountName = arrAccount[0];
+				Promise.reject("There was a problem with account selection. Transit was unable to detect which account was selected from within WhaleVault");
 			}
 
-			if (wv.whalevault) {
-				const pkResponse = await wv.whalevault.promiseRequestPubKeys(wv.appId, wv.accountChain+':'+wv.accountName);
+			wvAccountKey = response.result[response.data.username].activePubkey;
+			keyMap.push({ accountName: wvAccountName, key: wvAccountKey })
 
-				if (pkResponse.success && pkResponse.result &&
-					pkResponse.result[wv.accountChain+':'+wv.accountName] &&
-					pkResponse.result[wv.accountChain+':'+wv.accountName].activePubkey) {
-
-					wv.accountPubKeys = pkResponse.result[wv.accountChain+':'+wv.accountName];
-					wv.accountPublickey = pkResponse.result[wv.accountChain+':'+wv.accountName].activePubkey;
-
-					return {
-						accountName: wv.accountName,
-						permission: 'active',
-						publicKey: wv.accountPublickey
-					};
-
-				} else throw new Error("activeKey not found in WhaleVault for "+wv.accountChain+':'+wv.accountName);
-			}
-			
-			throw new Error('WhaleVault not Found!');
+			return {
+				accountName: wvAccountName,
+				permission: 'active',
+				publicKey: wvAccountKey
+			};
 
 		}
 
 		function logout(accountName?: string): Promise<any> {
-			wv.accountPublickey = '';
-			wv.accountPubKeys = '';
-			wv.accountChain = '';
-			wv.accountName = '';
-			wv.appId = '';
+			keyMap = [];
+
 			const res = async function m2(): Promise<any> {
 				if (true) return true;
 			};
@@ -200,13 +115,70 @@ export function whalevaultWalletProvider() {
 				shortName: 'WhaleVault',
 				description: 'WhaleVault :: Secure Graphene Cross-Chain Key Store Extension 🐳'
 			},
-			signatureProvider: makeSignatureProvider(network),
+			signatureProvider: {
+				
+					async getAvailableKeys() {
+						logIfDebugEnabled('In getAvailableKeys');
+			
+						let arr = keyMap.map((a) => a.key);
+						logIfDebugEnabled(JSON.stringify(arr));
+						return arr;
+						
+					},
+			
+					async sign(
+						signatureProviderArgs: ApiInterfaces.SignatureProviderArgs,
+						reason?: string
+					): Promise<RpcInterfaces.PushTransactionArgs> {
+						logIfDebugEnabled('In Sign');
+			
+						const rpc = new JsonRpc(network.protocol + '://' + network.host + ':' + network.port);
+						const args = { rpc, 
+									   authorityProvider: undefined, 
+									   abiProvider: undefined, 
+									   signatureProvider: this, 
+									   chainId: undefined, 
+									   textEncoder: undefined, 
+									   textDecoder: undefined
+									 };
+						const api = new Api(args);
+						const _txn = await api.deserializeTransactionWithActions(signatureProviderArgs.serializedTransaction);
+
+						console.log(_txn);
+			
+						if (reason == null || reason === '') reason = _txn.actions[0].name;
+			
+						_txn.network = network;
+			
+						const response = await whalevault.promiseRequestSignBuffer(appId, 'eos:'+_txn.actions[0].data.from, 
+																					  _txn, 'active', reason, 'eos');
+			
+						const signatureArray = response.success ? [response.result] : [];
+						const sigResponse: RpcInterfaces.PushTransactionArgs = {
+							signatures: signatureArray,
+							serializedTransaction: signatureProviderArgs.serializedTransaction
+						};
+			
+						return sigResponse;
+					}
+				
+			},
 			connect,
 			discover,
 			disconnect,
 			login,
 			logout,
-			signArbitrary
+			signArbitrary: function signArbitrary(data: string, userMessage: string): Promise<string> {
+				return new Promise((resolve, reject) => {
+					if (appId === '') reject('uninitalized');
+					// This is a bit of a hack. We don't know which account is requesting the arb signature so 1st account selected. Need to update transit framework to support. 
+					whalevault.requestSignBuffer(appId, 'eos:'+keyMap[0].accountName, data, 'active',
+													userMessage, 'eos', (response: any) => {
+						if (response.success) resolve(response.result); else reject(response.message);
+					});
+				});
+			}
+
 		};
 
 		return walletProvider;
